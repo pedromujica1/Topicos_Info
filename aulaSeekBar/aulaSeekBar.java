d60e86babfbe9c395f7bc6a362a4760bf68e52fb
Pedro Miotto Mujica <107735941+pedromujica1@users.noreply.github.com>
2023-06-15 08:38:38 -0300

    Update aulaSeekBar.java

--- aulaSeekBar/aulaSeekBar.java
+++ aulaSeekBar/aulaSeekBar.java
@@ -1,1 +1,41 @@
-aaaaa
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private SeekBar seekBarEscala;
+    private TextView textResultado;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBarEscala = findViewById(R.id.seekBarEscala);
+        textResultado = findViewById(R.id.textResultado);
+
+        seekBarEscala.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textResultado.setText("Progresso: "+ progress + "/"+ seekBar.getMax());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                textResultado.setText("On start Tracking Touch");
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textResultado.setText("On stop Tracking touch");
+
+            }
+        });
+
+    }
+
+}
