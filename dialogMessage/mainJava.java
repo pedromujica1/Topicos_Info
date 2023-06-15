c90d2c4b27aadd1572e49c0514af09747eabd69c
Pedro Miotto Mujica <107735941+pedromujica1@users.noreply.github.com>
2023-06-15 08:14:52 -0300

    Update mainJava.java

--- dialogMessage/mainJava.java
+++ dialogMessage/mainJava.java
@@ -1,4 +1,4 @@
-
+package com.example.aulacheckbox;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,16 +20,10 @@ import com.github.chrisbanes.photoview.PhotoView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
     }
 
@@ -37,8 +31,14 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
 
         //configurar titulo e mensagem
-        dialog.setTitle("Titúlo da dialog");
-        dialog.setMessage("mensagem da dialog");
+        dialog.setTitle("Dialog title");
+        dialog.setMessage("Dialog message");
+
+        //configurar cancelemaneto do alertDialog
+        dialog.setCancelable(false);
+
+        //Configurar ícone
+        dialog.setIcon(android.R.drawable.ic_btn_speak_now);
 
         //configurar açoes para sim e nâo
         dialog.setPositiveButton("sim", new DialogInterface.OnClickListener() {
@@ -49,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //configura ação de não
         dialog.setNegativeButton("nao", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -56,13 +57,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
         dialog.create();
         dialog.show();
-
-
     }
-
-
-
 }
